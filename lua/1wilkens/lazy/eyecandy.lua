b192cd50d230dfb806e96a206b16d94689736f1a
Florian Wilkens <gh@1wilkens.org>
2025-11-22 01:21:36 +0100

    lazy/eyecandy: base16-vim -> tinted-nvim

--- lua/1wilkens/lazy/eyecandy.lua
+++ lua/1wilkens/lazy/eyecandy.lua
@@ -1,6 +1,29 @@
 return {
-    -- base16
-    "chriskempson/base16-vim",
+    -- tinted-nvim
+    {
+        "tinted-theming/tinted-nvim",
+
+        config = function()
+            require("tinted-colorscheme").setup({
+                supports = {
+                    tinty = true,
+                    tinted_shell = true,
+                },
+                highlights = {
+                    telescope = true,
+                    telescope_borders = false,
+                    indentblankline = true,
+                    notify = true,
+                    cmp = true,
+                    ts_rainbow = true,
+                    illuminate = true,
+                    lsp_semantic = true,
+                    mini_completion = true,
+                    dapui = true,
+                },
+            })
+        end,
+    },
 
     -- lightline
     "itchyny/lightline.vim",
